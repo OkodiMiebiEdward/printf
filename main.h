bab5273410fd280e47d3f9c7ae4fab09f6e39a4a
OkodiMiebiEdward <miebiokodiedward@gmail.com>
2023-04-24 06:57:58 -0700

    added the binary_to_tmp prototype

--- main.h
+++ main.h
@@ -14,6 +14,7 @@ void str_to_temp(char temp[], int *index, char *str);
 void number_to_temp(char temp[], int *index, int num);
 int check_specifier(char c);
 int _printf(char *format, ...);
+void binary_to_tmp(char temp[], int *index, unsigned int num);
 int determine_specifier(char temp[], int *index, va_list args, char format);
 
 #endif
